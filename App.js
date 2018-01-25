6c14e97f4a7febf859ca7ed642ab0a28053847bb
SanBunny <bunnysancambodia@gmail.com>
2018-01-25 13:08:53 +0700

    sample code

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from "react";
+import Setup from "./src/boot/setup";
+
+export default class App extends React.Component {
+  render() {
+    return <Setup />;
+  }
+}
